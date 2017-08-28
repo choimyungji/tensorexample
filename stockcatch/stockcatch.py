4ce4147dea436daff183b13aa10422979fdaf7f6
maengji <maeng@MaengMBPR.local>
2017-08-28 23:04:13 +0900

    xml parsing

--- stockcatch/stockcatch.py
+++ stockcatch/stockcatch.py
@@ -1,8 +1,11 @@
 import urllib.request
+import xml.etree.ElementTree as ET
 
 if __name__ == "__main__":
     urlstring = "http://asp1.krx.co.kr/servlet/krx.asp.XMLSise?code=035420"
     req = urllib.request.Request(urlstring)
-    data = urllib.request.urlopen(req).read()
+    data = urllib.request.urlopen(req).read().decode("utf-8")
 
     print (data)
+    tree = ET.fromstring(data.replace('\n',''))
+    print(tree)
