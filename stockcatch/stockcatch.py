d5740e8641fb3944cc763a8cbd30a119a3e0fd47
maengji <maeng@MaengMBPR.local>
2017-08-29 23:31:53 +0900

    add stocks

--- stockcatch/stockcatch.py
+++ stockcatch/stockcatch.py
@@ -2,10 +2,12 @@ import urllib.request
 import xml.etree.ElementTree as ET
 
 if __name__ == "__main__":
-    urlstring = "http://asp1.krx.co.kr/servlet/krx.asp.XMLSise?code=035420"
-    req = urllib.request.Request(urlstring)
-    data = urllib.request.urlopen(req).read().decode("utf-8")
+    codes = ['035420', '054920', '015760']
+    for code in codes:
+        urlstring = "http://asp1.krx.co.kr/servlet/krx.asp.XMLSise?code=" + code
+        req = urllib.request.Request(urlstring)
+        data = urllib.request.urlopen(req).read().decode("utf-8")
 
-    print (data)
-    tree = ET.fromstring(data.replace('\n',''))
-    print(tree)
+        # print (data)
+        tree = ET.fromstring(data.replace('\n',''))
+        print(tree.find('TBL_StockInfo').attrib['JongName'],tree.find('TBL_StockInfo').attrib['CurJuka'], )
