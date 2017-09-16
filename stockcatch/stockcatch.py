570691854c660d68e8176649251e6a426b9e58a9
maengji <choimyungji@gmail.com>
2017-09-16 23:41:55 +0900

    read all stocks

--- stockcatch/stockcatch.py
+++ stockcatch/stockcatch.py
@@ -2,12 +2,13 @@ import urllib.request
 import xml.etree.ElementTree as ET
 
 if __name__ == "__main__":
-    codes = ['035420', '054920', '215600']
+    codes = ["%.6d" % i for i in range(999999)]
+    # codes = ['035420', '054920', '215600']
     for code in codes:
         urlstring = "http://asp1.krx.co.kr/servlet/krx.asp.XMLSise?code=" + code
         req = urllib.request.Request(urlstring)
         data = urllib.request.urlopen(req).read().decode("utf-8")
 
-        # print (data)
         tree = ET.fromstring(data.replace('\n',''))
-        print(tree.find('TBL_StockInfo').attrib['JongName'],tree.find('TBL_StockInfo').attrib['CurJuka'], )
+        if tree.find('TBL_StockInfo').attrib['JongName'] != "":
+            print(code, tree.find('TBL_StockInfo').attrib['JongName'],tree.find('TBL_StockInfo').attrib['CurJuka'] )
